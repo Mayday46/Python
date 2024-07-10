5907a9ef6e8230ca35e34f1bed58818541c50474
Mayday46 <81172807+Mayday46@users.noreply.github.com>
2024-07-09 23:34:37 -0400

    main.py

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,9 +1,23 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
+from pprint import pprint
 import notification_manager
 import flight_data
 import flight_search
 
 data_Manger = DataManager()
-SHEET_DATA = data_Manger.priceData
-print(SHEET_DATA)
+SHEET_DATA = data_Manger.fetch_data()
+# print(len(SHEET_DATA)) # -> 9
+pprint(SHEET_DATA)
+print()
+
+# Testing assignment to iataCode
+for eachItem in SHEET_DATA:
+    data_Manger.update_iata_code(data = SHEET_DATA, iata_code = 'TESTING')
+
+for each in range(2, len(SHEET_DATA) + 2):
+    data_Manger.sheety_put(iata_Code = "TESTING" , rowCode= each)
+
+print()
+pprint(SHEET_DATA)
+print()
