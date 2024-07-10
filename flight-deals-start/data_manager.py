a59e18a8ed9f2d85c4da22dd32d35ee568ccda75
Mayday46 <81172807+Mayday46@users.noreply.github.com>
2024-07-09 23:34:09 -0400

    data_manager.py

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,19 +1,33 @@
 import requests
 from pprint import pprint
 from Credentials import SHEETY_FLIGHT_INFO_URL
-
+from Credentials import SHEETY_FLIGHT_GOOGLE_SPREADSHEETS
 
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    priceData = None
-    URL = SHEETY_FLIGHT_INFO_URL
-    response = requests.get(url = URL)
-    if response.status_code == 200:
+    # This class is responsible for talking to the Google Sheet.
+
+    def fetch_data(self): # This function returns all the flight offer.
+        response = requests.get(url = SHEETY_FLIGHT_INFO_URL)
         data = response.json()
-        # print(data['prices'])
-        # print(data)
-        # pprint(data)
-        # pprint(data['prices'])
-        priceData = data['prices']
-    else:
-        print(f"Failed to fetch data: {response.status_code}")
+        return data['prices']
+
+    def update_iata_code(self, data, iata_code):
+        for item in data:
+            # Check if the iataCode is empty, if it is modify...
+            if item['iataCode'] == "":
+                item['iataCode'] = iata_code
+            else:
+                continue
+                
+
+    def get_data(self, data):
+        return data
+    
+    def sheety_put(self, iata_Code, rowCode):
+        URL = SHEETY_FLIGHT_GOOGLE_SPREADSHEETS.replace("[Object ID]", str(rowCode))
+        body = {
+            "price": {
+                "iataCode": iata_Code
+            }
+        }
+        response = requests.put(url = URL, json=body)
