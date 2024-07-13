4db4d818a260940db06b2609a67048a0ab845518
Mayday46 <81172807+Mayday46@users.noreply.github.com>
2024-07-12 23:14:04 -0400

    Update LinkedList.py

--- Data Structures/LinkedList/LinkedList.py
+++ Data Structures/LinkedList/LinkedList.py
@@ -3,21 +3,33 @@ from Node import Node
 class LinkedList:
 
     def __init__(self):
-        self.head = None # Initially, there is nothing in the list.
+        self.head = None
 
-    def insertionAtEnd(self, data):
-        newNode = Node(data)
+    def traverse(self):
+        currNode = self.head
+        while currNode is not None:
+            print(currNode.value)
+            currNode = currNode.next
+    
+    def append(self, data): # Function add node to last.
+        new_node = Node(data)
         if self.head is None:
-            # If there is empty linked list, then make the newNode as the head of our
-            # new linked list.
-            self.head = newNode
+            self.head = new_node  # If the list is empty, set the head as the new node
         else:
-            currNode = self.head
-            while currNode.next is not None:
-                currNode = currNode.next
-            currNode.next = newNode
-    # DummyNode -> B -> C | Add A
-    def insertionAtHead(self, data):
+            current = self.head
+            while current.next:  # Traverse to the end of the list
+                current = current.next
+            current.next = new_node  # Set the next attribute of the last node to the new node
+
+    def length(self):
+        count = 0
+        currNode = self.head
+        while currNode is not None:
+            count += 1
+            currNode = currNode.next
+        return count
+    
+    def insert_Beginning(self, data):
         # b -> c
         new_Node = Node(data)
         if self.head is None:
@@ -26,18 +38,85 @@ class LinkedList:
         else:
             new_Node.next = self.head
             self.head = new_Node
-
-    def remove(self, target):
-        if self.head is None:
-            return "List Empty"
-        while self.head and self.head.data == target:
-            self.head = self.head.next
-
-        prevNode = self.head
+    
+    def insert_after(self, afterValue, insertingValue):
+        # afterValue = B
+        # insertingValue = C
+        # A -> B -> D
+        newNode = Node(insertingValue)
         currNode = self.head
 
+        # while currNode:
+        #     if currNode.value != afterValue:
+        #         prevNode = currNode
+        #         currNode = currNode.next
+        #     else:
+        #         newNode.next = currNode.next
+        #         currNode.next = newNode
         while currNode:
-            if currNode.data != target:
+            if currNode.value == afterValue:
+                newNode.next = currNode.next
+                currNode.next = newNode
+                return  # Exit after inserting the node
+            currNode = currNode.next
+        
+    def last_Node(self):
+        currNode = self.head
+        lastNode = None
+        while currNode is not None:
+            lastNode = currNode.value
+            currNode = currNode.next
+        return lastNode
+
+    # A -> B -> C -> None
+    def delete_last(self):
+        if self.head is None: 
+            # Handles the edge case where the list is empty
+            return None
+        
+        elif self.head.next is None:
+            # Handles the case where there is only one element in the list
+            self.head = None
+            return
+        
+        else:
+            currNode, prevNode = self.head, None
+            while currNode.next is not None:
+                prevNode = currNode
+                currNode = currNode.next
+            prevNode.next = None
+
+    # A -> B -> C -> None | Del A
+    def delete_first(self):
+        if self.head is None:
+            # Handles the edge case where the list is empty
+            return None
+        
+        if self.head.next is None:
+            # Handles the case where there is only one element in the list
+            self.head = None
+            return
+        
+        # Handles the general case
+        self.head = self.head.next
+
+    def find(self, target):
+        currNode = self.head
+        while currNode:
+            if currNode.value == target:
+                return True
+            currNode = currNode.next
+        return False
+    
+    def remove_this(self, target):
+        # A -> B -> C -> D
+        if self.head is None:
+            return "List Empty"
+        while self.head and self.head.value == target:
+            self.head = self.head.next
+        prevNode, currNode = self.head, self.head
+        while currNode:
+            if currNode.value != target:
                 prevNode = currNode
                 currNode = currNode.next
             else:
@@ -48,37 +127,30 @@ class LinkedList:
         return self.head
 
     def removeAtIndex(self, index):
-
         if index == 0:
             self.head = self.head.next
-
-        currNode = self.head
-        prevNode = self.head
+        
+        currNode, prevNode = self.head, self.head
         counter = 0
+        # 1 -> 2 -> 3 -> 4
         while currNode:
             if counter != index:
                 counter += 1
                 prevNode = currNode
                 currNode = currNode.next
             else:
-                # counter == index
                 prevNode.next = currNode.next
                 currNode = currNode.next
 
-        return self.head
 
-    def listTraversal(self):
-        tempNode = self.head
-        while tempNode is not None:
-            print(tempNode.data)
-            tempNode = tempNode.next
-
-    def getValues(self):
-        # Returns an array of all the values in the linked list, ordered from head to tail
+    def is_empty(self):
+        return self.head is None
+    
+    def allValues(self):
         currNode = self.head
-        resultArray = []
+        res = []
         while currNode:
-            if currNode.data:
-                resultArray.append(currNode.data)
+            if currNode.value:
+                res.append(currNode.value)
             currNode = currNode.next
-        return resultArray
+        return res
