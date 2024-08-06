2047df419a57c7e59d1511152b75827243837e8f
Mayday46 <81172807+Mayday46@users.noreply.github.com>
2024-08-06 12:14:25 -0400

    Add files via upload

---
+++ Leetcode/symmetricTree.py
@@ -0,0 +1,53 @@
+'''
+
+101. Symmetric Tree
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+    - Input: root = [1, 2, 2, 3, 4, 4, 3]
+    - Ouput: True
+
+Example 2:
+    - Input: root = [1, 2, 2, null, 3, null, 3]
+    - Ouput: false
+
+
+'''
+
+
+
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val, left = None, right = None):
+        self.val = val 
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        def isMirror(left: TreeNode, right: TreeNode) -> bool:
+            
+            if not left and not right: # Case 1: If the root is null
+                return True
+            
+            if not left or not right: # Case 2: If either left or right subtree is null, it not symmetric
+                return False
+            
+            # Case 3: The root is not null, and it both has a left and right subtree
+            if left.val != right.val:
+                return False
+            
+            return (isMirror(left.left, right.right) and isMirror(left.right, right.left))
+        
+        return isMirror(root.left, root.right) if root else True
+
+root = TreeNode(1)
+root.left = TreeNode(2, TreeNode(3), TreeNode(4))
+root.right = TreeNode(2, TreeNode(4), TreeNode(3))
+
+solution = Solution()
+print(solution.isSymmetric(root))  # Output: True
